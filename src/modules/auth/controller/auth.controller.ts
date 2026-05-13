612787e18ec889b62e325022d8597f6dbdd79d45
nabila-momtarin <nabila.momtarin.me@gmail.com>
2026-05-13 18:20:58 +0600

    feat: add meTest endpoint to fetch current user details

--- src/modules/auth/controller/auth.controller.ts
+++ src/modules/auth/controller/auth.controller.ts
@@ -1,14 +1,16 @@
-import { Body, Controller, Logger, Post } from "@nestjs/common";
+import { Body, Controller, Get, Logger, Post } from "@nestjs/common";
 import { LoginDto } from "../dto/login.dto";
 import { AuthService } from "../service/auth.service";
 import { Public } from "src/infrastructure/auth/decorators/public.decorator";
+import { CurrentUser } from "src/common/decorators/current-user.decorator";
+import { AuthenticatedUser } from "src/infrastructure/auth/types/auth.types";
 
 
 
 
 @Controller('login')
 export class AuthController {
-    constructor( private readonly authService: AuthService) {}
+    constructor(private readonly authService: AuthService) { }
 
     private logger = new Logger(AuthController.name);
 
@@ -18,5 +20,14 @@ export class AuthController {
         this.logger.log('CONTROLLER : login\n');
 
         return this.authService.login(dto);
+    }
+
+    @Get('me-test')
+    meTest(@CurrentUser() user: AuthenticatedUser) {
+        return {
+            success: true,
+            message: 'Current user fetched successfully',
+            data: user,
+        };
     }
 }
