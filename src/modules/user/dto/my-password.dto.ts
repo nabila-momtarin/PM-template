640e2cdb2ecf8016a0cb36139c9d6a9381cc7c75
nabila-momtarin <nabila.momtarin.me@gmail.com>
2026-06-24 12:23:04 +0600

    refactor(password): comment out strong password validation for currentPassword field

--- src/modules/user/dto/my-password.dto.ts
+++ src/modules/user/dto/my-password.dto.ts
@@ -5,13 +5,13 @@ import { IsNotEmpty, IsString, IsStrongPassword } from "class-validator";
 export class ChangePasswordDto {
     @IsNotEmpty()
     @IsString()
-    @IsStrongPassword({
-        minLength: 8,
-        minLowercase: 0,
-        minUppercase: 1,
-        minNumbers: 1,
-        minSymbols: 1,
-    })
+    // @IsStrongPassword({
+    //     minLength: 8,
+    //     minLowercase: 0,
+    //     minUppercase: 1,
+    //     minNumbers: 1,
+    //     minSymbols: 1,
+    // })
     currentPassword: string;
 
 
